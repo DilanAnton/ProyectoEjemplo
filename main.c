f1a9b9ea514dd3ffe0744d90bc6ba52833c3f2bb
DilanAnton <u202312076@upc.edu.pe>
2025-08-30 18:21:21 -0500

    dev1: documentacion

--- main.c
+++ main.c
@@ -1,3 +1,8 @@
 int main (void){
     printf("Hello, World!");
+    // git clone <repo>
+    // git fetch
+    // git pull origin main
+    // git commit -am "mensaje"
+    // git push origin main
 }
